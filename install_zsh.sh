6bb362dd9e1cdbf9bc88b336a1c86f35ae6b53f4
PARROTFLAKES <gdrakemail@gmail.com>
2021-11-01 22:18:55 -0400

    + install scripts

---
+++ install_zsh.sh
@@ -0,0 +1,7 @@
+# zsh
+sudo apt install zsh
+# oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# replace configuration file
+rm ~/.zshrc
+ln ~/.config/zsh/.zshrc ~/.zshrc
