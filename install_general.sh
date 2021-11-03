16a0f600ded1ca6ded9de1339efd58b0838ecca4
PARROTFLAKES <gdrakemail@gmail.com>
2021-11-03 19:17:16 -0400

    + y/n prompts

--- install_general.sh
+++ install_general.sh
@@ -1,32 +1,88 @@
-sudo apt update
+echo
+read -p "Update APT cache? [y/n] " MSG
+if [ "$MSG" == "yes" ] || [ "$MSG" == "y" ]; then
+	sudo apt update
+fi
 
-# basics
-sudo apt install build-essential # gcc + more libraries
-sudo apt install git-all
-sudo apt install tree
-sudo apt-get install youtube-dl # media downloader
+echo
+read -p "Install BASICS package? [y/n] " MSG
+if [ "$MSG" == "yes" ] || [ "$MSG" == "y" ]; then
+	sudo apt install build-essential # gcc + more libraries
+	sudo apt install git-all
+	sudo apt install tree
+	sudo apt-get install youtube-dl # media downloader
+fi
 
-# zsh
-sudo apt install zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-rm -f ~/.zshrc
-ln ~/.config/zsh/.zshrc ~/.zshrc
+echo
+read -p "Install ZSH package? [y/n] " MSG
+if [ "$MSG" == "yes" ] || [ "$MSG" == "y" ]; then
+	sudo apt install zsh
+	sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+	rm -f ~/.zshrc
+	ln ~/.config/zsh/.zshrc ~/.zshrc
+fi
 
-# neovim
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
+echo
+read -p "Install NEOVIM package? [y/n] " MSG
+if [ "$MSG" == "yes" ] || [ "$MSG" == "y" ]; then
+	sudo add-apt-repository ppa:neovim-ppa/unstable
+	sudo apt-get update
+	sudo apt-get install neovim
+fi
 
-# js
-sudo apt install npm
-sudo apt install nodejs
+echo
+read -p "Install JS package? [y/n] " MSG
+if [ "$MSG" == "yes" ] || [ "$MSG" == "y" ]; then
+	sudo apt install npm
+	sudo apt install nodejs
+fi
 
-# rust
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+echo
+read -p "Install RUST package? [y/n] " MSG
+if [ "$MSG" == "yes" ] || [ "$MSG" == "y" ]; then
+	curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+fi
 
-# python
-sudo apt install python3-pip
+echo
+read -p "Install PYTHON package? [y/n] " MSG
+if [ "$MSG" == "yes" ] || [ "$MSG" == "y" ]; then
+	sudo apt install python3-pip
+fi
 
-# absolutely unnecessary
-sudo apt get cmatrix
-sudo apt install nethack-console
+echo
+read -p "Install FUNTIMES package? [y/n] " MSG
+if [ "$MSG" == "yes" ] || [ "$MSG" == "y" ]; then
+	sudo apt get cmatrix
+	sudoapt install nethack-console
+fi
+
+# # basics
+# sudo apt install build-essential # gcc + more libraries
+# sudo apt install git-all
+# sudo apt install tree
+# sudo apt-get install youtube-dl # media downloader
+
+# # zsh
+# sudo apt install zsh
+# sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# rm -f ~/.zshrc
+# ln ~/.config/zsh/.zshrc ~/.zshrc
+
+# # neovim
+# sudo add-apt-repository ppa:neovim-ppa/unstable
+# sudo apt-get update
+# sudo apt-get install neovim
+
+# # js
+# sudo apt install npm
+# sudo apt install nodejs
+
+# # rust
+# curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+
+# # python
+# sudo apt install python3-pip
+
+# # absolutely unnecessary
+# sudo apt get cmatrix
+# sudoapt install nethack-console
