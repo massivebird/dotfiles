c674af72848344df5fabeec66648c0eefb78f80b
PARROTFLAKES <gdrakemail@gmail.com>
2021-11-03 19:19:15 -0400

    install_general now universal

--- install_zsh.sh
+++
@@ -1,7 +0,0 @@
-# zsh
-sudo apt install zsh
-# oh-my-zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-# replace configuration file
-rm -f ~/.zshrc
-ln ~/.config/zsh/.zshrc ~/.zshrc
