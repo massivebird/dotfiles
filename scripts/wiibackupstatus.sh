0a2d5770b1cae7b42dfee60407c6c404c9a73ecb
massivebird <gdrakemail@gmail.com>
2022-02-13 15:06:39 -0500

    + colored and cleaner output

--- scripts/wiibackupstatus.sh
+++ scripts/wiibackupstatus.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
-# Output colors
-# printf "I ${RED}love${NC} Stack Overflow\n"]']'
-NC='\033[0m' # No Color
-RED='\033[0;31m'
+# Output formatting
+CYAN="$(tput setaf 45)"
+GREEN="\033[32m"
+MAGENTA="$(tput setaf 99)"
+RED="$(tput setaf 1)"
+NC="$(tput sgr 0)"
+
+WII="${CYAN}WII${NC}"
+GCN="${MAGENTA}GCN${NC}"
 
 # Paths to Wii and GCN directories
 wiigames=/mnt/*/wiiback/wbfs
 gcngames=/mnt/*/wiiback/Games
 
-# Returns unexpected filenames
+# Returns unexpected filename(s)
 unexwii=$(ls $wiigames/*/* | grep -vE '\[([A-Z0-9]{6})\]\/\1\.wbf[s0-9]')
 unexgcn=$(ls $gcngames/*/* | grep -vE '\[([A-Z0-9]{6})\]\/game\.iso')
 
 if [ $(echo $unexwii | wc -w) -gt 0 ]; then
-	echo "Unexpected Wii filenames detected:"
-	echo -e $unexwii "\n"
+	echo -e "[$RED OHNO $NC] $WII: Unexpected filename(s) detected"
+	echo -e $unexwii
 else
-	echo -e "Wii games look good!\n"
+	echo -e "[$GREEN  OK  $NC] $WII: Looking good!"
 fi
 
 if [ $(echo $unexgcn | wc -w) -gt 0 ]; then
-	echo "Unexpected GCN filenames detected:"
+	echo -e "\n[$RED OHNO $NC] $GCN: Unexpected filename(s) detected"
 	echo $unexgcn
 else
-	echo "GCN games look good!"
+	echo -e "\n[$GREEN  OK  $NC] $GCN: Looking fresh!"
 fi
