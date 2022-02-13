b9adcbca844b9146cfa23c23955b81e117873d46
massivebird <gdrakemail@gmail.com>
2022-02-13 18:14:36 -0500

    + supports checking wii drive as well as backup + comments + newlines for aesthetics

--- scripts/wiibackupstatus.sh
+++ scripts/wiibackupstatus.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# Output formatting
+# Color presets
 CYAN="$(tput setaf 45)"
-GREEN="\033[32m"
+GREEN="$(tput setaf 34)"
 MAGENTA="$(tput setaf 99)"
 RED="$(tput setaf 1)"
+YELLOW="$(tput setaf 220)"
 NC="$(tput sgr 0)"
 
+# Colored Wii and GCN strings
 WII="${CYAN}WII${NC}"
 GCN="${MAGENTA}GCN${NC}"
 
-# Paths to Wii and GCN directories
-wiigames=/mnt/*/wiiback/wbfs
-gcngames=/mnt/*/wiiback/Games
+echo # print newline
+
+# Paths to Wii and GCN directories based on optional argument
+if [ $(echo $1 | wc -w) -eq 0 ]; then
+	wiigames=/mnt/*/wiiback/wbfs
+	gcngames=/mnt/*/wiiback/games
+elif [ -d "/mnt/$1/wbfs" ] && [ -d "/mnt/$1/games" ]; then
+	wiigames=/mnt/$1/wbfs
+	gcngames=/mnt/$1/games
+else
+	echo -e "[$YELLOW WARN $NC] Drive ${1^^} is not your Wii drive! Defaulting to" /mnt/*/wiiback "\b."
+	wiigames=/mnt/*/wiiback/wbfs
+	gcngames=/mnt/*/wiiback/games
+fi
 
 # Returns unexpected filename(s)
 unexwii=$(ls $wiigames/*/* | grep -vE '\[([A-Z0-9]{6})\]\/\1\.wbf[s0-9]')
@@ -26,8 +39,10 @@ else
 fi
 
 if [ $(echo $unexgcn | wc -w) -gt 0 ]; then
-	echo -e "\n[$RED OHNO $NC] $GCN: Unexpected filename(s) detected"
+	echo -e "[$RED OHNO $NC] $GCN: Unexpected filename(s) detected"
 	echo $unexgcn
 else
-	echo -e "\n[$GREEN  OK  $NC] $GCN: Looking fresh!"
+	echo -e "[$GREEN  OK  $NC] $GCN: Looking fresh!"
 fi
+
+echo # print newline
