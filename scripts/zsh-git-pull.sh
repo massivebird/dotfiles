43ec3743da4ef18de013965da2662b2e7f55da51
massivebird <gdrakemail@gmail.com>
2022-03-19 15:23:23 -0400

    + comments

--- scripts/zsh-git-pull.sh
+++ scripts/zsh-git-pull.sh
@@ -30,28 +30,38 @@ done
 
 # Main function
 source_repo () {
-	# Arguments -> readable variables
+	# Declaring readable variables
 	REPO_PATH=$1
 	REPO_LABEL=$2
 	REPO_BRANCH="$(git -C $REPO_PATH branch -a | grep -Po '(?<=\*\ ).*')"
-	# Check if path exists
+	# Check if repo path exists
+	# If it does...
 	if [ -d $REPO_PATH ]; then
-		# If path exists,
-		# check if it needs updating
+		# ...  `git fetch` to get latest remote commits
 		git -C $REPO_PATH fetch -q origin $REPO_BRANCH
+		# Store differences between newest local and newest remote commits
 		DIFF_DUMP=$(git -C $REPO_PATH diff $REPO_BRANCH origin/$REPO_BRANCH)
+		# If there are differences...
 		if [ -n "$DIFF_DUMP" ]; then
+			# ... inform user that it's being updated
 			echo "$STATUS_CONS Updating $REPO_LABEL..."
 		fi
+		# Oh, and `git pull` and store its errors
 		ERROR_DUMP=$(git -C $REPO_PATH pull -q origin $REPO_BRANCH 2>&1)
+	# If path does not exist...
 	else
+		# ... inform user and exit the function
 		echo "$STATUS_OHNO $REPO_LABEL not found."
+		return
 	fi
-	# Print vague error if pull fails
+	
+	# If previous `git pull` produced an error...
 	if [ -n "$ERROR_DUMP" ]; then
+		# ... inform user that the update failed
 		echo "$STATUS_OHNO $REPO_LABEL failed to pull"
-	# If -v, print message on successful pull
+	# If -v...
 	elif [ -n "$FLAG_VERBOSE" ]; then
+		# ... inform user that the update succeeded
 		echo "$STATUS_COOL $REPO_LABEL up to date! [$REPO_BRANCH]"
 	fi
 }
