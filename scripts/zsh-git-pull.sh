614ed0ca10531ebd8a574bb62acd65c2c3ecacfc
massivebird <gdrakemail@gmail.com>
2022-03-19 15:09:55 -0400

    ~ I fixed EVERYTHING AGAIN (ACTUALLY WORKS)

--- scripts/zsh-git-pull.sh
+++ scripts/zsh-git-pull.sh
@@ -33,25 +33,26 @@ source_repo () {
 	# Arguments -> readable variables
 	REPO_PATH=$1
 	REPO_LABEL=$2
-	ERROR_DUMP="empty"
-	REPO_BRANCH=\[$(git -C $REPO_PATH branch -a | grep -Po '(?<=\*\ ).*')\]
+	REPO_BRANCH="$(git -C $REPO_PATH branch -a | grep -Po '(?<=\*\ ).*')"
 	# Check if path exists
 	if [ -d $REPO_PATH ]; then
 		# If path exists,
 		# check if it needs updating
-		if [ -n $(git -C $REPO_PATH diff $REPO_BRANCH origin/$REPO_BRANCH) ]; then
-			echo "$STATUS_CONS Updating $REPO_LABEL..." 1> /dev/null
+		git -C $REPO_PATH fetch -q origin $REPO_BRANCH
+		DIFF_DUMP=$(git -C $REPO_PATH diff $REPO_BRANCH origin/$REPO_BRANCH)
+		if [ -n "$DIFF_DUMP" ]; then
+			echo "$STATUS_CONS Updating $REPO_LABEL..."
 		fi
-		git -C $REPO_PATH pull 1> /dev/null 2> "$ERROR_DUMP"
+		ERROR_DUMP=$(git -C $REPO_PATH pull -q origin $REPO_BRANCH 2>&1)
 	else
 		echo "$STATUS_OHNO $REPO_LABEL not found."
 	fi
 	# Print vague error if pull fails
-	if [ "$ERROR_DUMP" != "empty" ]; then
+	if [ -n "$ERROR_DUMP" ]; then
 		echo "$STATUS_OHNO $REPO_LABEL failed to pull"
 	# If -v, print message on successful pull
 	elif [ -n "$FLAG_VERBOSE" ]; then
-		echo "$STATUS_COOL $REPO_LABEL up to date! $REPO_BRANCH"
+		echo "$STATUS_COOL $REPO_LABEL up to date! [$REPO_BRANCH]"
 	fi
 }
 
