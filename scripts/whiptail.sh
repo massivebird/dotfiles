2b9e646e6bb468c81e1acc7838fa8fde17ecb359
PARROTFLAKES <gdrakemail@gmail.com>
2022-03-26 10:24:46 -0400

    + choice identifier

--- scripts/whiptail.sh
+++ scripts/whiptail.sh
@@ -11,9 +11,9 @@ button=black,black
 '
 
 CHOICE=$(whiptail --checklist "Choose your packages!" 18 80 10 \
-	"UPAPT" "Update apt cache (recommended)" OFF \
+	"UPAPT" "Update apt cache (recommended)" ON \
 	"INBASICS" "Basics (git, tree, rename, ...)" OFF \
-	"INZSH" "ZSH" OFF \
+	"INZSHSHELL" "ZSH" OFF \
 	"INZSHPLUG" "ZSH Plugins" OFF \
 	"INVIM" "Neovim" OFF \
 	"INJS" "JavaScript (npm, nodejs)" OFF \
@@ -22,8 +22,33 @@ CHOICE=$(whiptail --checklist "Choose your packages!" 18 80 10 \
 	"INCLJ" "Clojure" OFF \
 	"INFUN" "Fun times! (cmatrix, nethack)" OFF 3>&1 1>&2 2>&3)
 
-if [ -z "$CHOICE"  ]; then
-	echo "No option was chosen (user hit Cancel)"
-else
-	echo "The user chose $CHOICE"
+if [[ "$CHOICE" == *"UPAPT"* ]]; then
+	echo "Update apt cache"
+fi
+if [[ "$CHOICE" == *"INBASICS"* ]]; then
+	echo "Install basics package"
+fi
+if [[ "$CHOICE" == *"INZSHSHELL"* ]]; then
+	echo "Install ZSH shell"
+fi
+if [[ "$CHOICE" == *"INZSHPLUG"* ]]; then
+	echo "Install ZSH plugins"
+fi
+if [[ "$CHOICE" == *"INVIM"* ]]; then
+	echo "Install neovim"
+fi
+if [[ "$CHOICE" == *"INJS"* ]]; then
+	echo "Install JavaScript"
+fi
+if [[ "$CHOICE" == *"INRS"* ]]; then
+	echo "Install Rust"
+fi
+if [[ "$CHOICE" == *"INPY"* ]]; then
+	echo "Install Python"
+fi
+if [[ "$CHOICE" == *"INCLJ"* ]]; then
+	echo "Install clojure"
+fi
+if [[ "$CHOICE" == *"INFUN"* ]]; then
+	echo "Install FUN STUFF"
 fi
