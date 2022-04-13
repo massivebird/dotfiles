7ea172402270a2dbe89bc93e96ac3b5e5b9054d7
massivebird <gdrakemail@gmail.com>
2022-04-12 20:59:22 -0400

    + chatterino-highlights.sh Takes a space-separated list of Twitch highlight keywords and turns it into a regex match

---
+++ scripts/chatterino-highlights.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+INP="""
+parrotflakes parrot perry pierre peppermint peru parrto peritot perdu periwinkle perimeter perineum parrots perrit passepartout parot puertorico parlayed purry parrrot praotot parsley parsnip paroot parru porrat parrotski sepowski Pavarotti parror parrty pattor porrot parro parruu Parrotsteakes chickenflakes parrpt parruto parroto badmacrobirb aprrot parrotwinkle pbaka pebbermint parrowinkle parur apprt paavarotti parrut prutt patrro prrot parrru parrbt parruru parruparru padbu parrout paurr paparru papagaio parrupan papaur parrit paparu pariiwinkle parron parotr shagaru pparu pypo birbeque parr prrato parvel perriwinkle pawwot parski partu parrof piro pierrot psrru pbrrot birdshavings puro poru pirru pilsk perru parto paparotto pervo parou parpo paraben parruwu parrowo partot parrat paru purrbot perrat pparru pawwu padu pertot pareot parri parroot poopiflakes pastebin
+"""
+
+# String manipulations
+echo $INP | \
+	# Remove leading/trailing whitespace
+	sed -E 's/^\ *|\ *$//' | \
+	# Turns spaces into booleans
+	sed 's/\ /|/g'
