43fa07170ae85ccd4dd6e65f46f16caa46682e77
massivebird <gdrakemail@gmail.com>
2022-05-07 12:58:16 -0400

    + fromhex.sh

---
+++ scripts/fromhex.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+## fromhex.sh
+# Converts colors from hex to tput
+#
+# Arguments :
+#	$1: six-character hex code
+
+# argument detection
+if [ -z "$1" ]; then
+	echo "$0: missing operand"
+	exit 1
+fi
+
+# http://unix.stackexchange.com/a/269085/67282
+hex=${1#"#"}
+r=$(printf '0x%0.2s' "$hex")
+g=$(printf '0x%0.2s' ${hex#??})
+b=$(printf '0x%0.2s' ${hex#????})
+printf '%03d' "$(( (r<75?0:(r-35)/40)*6*6 + 
+(g<75?0:(g-35)/40)*6   +
+(b<75?0:(b-35)/40)     + 16 ))"
+printf "\n"
+exit 0
