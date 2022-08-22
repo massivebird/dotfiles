697a053c58e67b410b425d7b1f2ec2dbc98cf6ca
massivebird <gdrakemail@gmail.com>
2022-08-22 02:46:21 -0400

    ~ housekeeping, organizing

--- scripts/lib/loading-spinner.sh
+++ scripts/lib/loading-spinner.sh
@@ -1,48 +1,47 @@
 #!/usr/bin/env bash
 
 ## loading-spinner.sh
-# Based on https://unix.stackexchange.com/a/225183
-# Generates loading spinner animation
-# for most recent background process
-#
-# ARGUMENTS :
-# $1: Loading message
-# $2: Done message
-# $3: Error message (requires $4)
-# $4: Error file (if file is non-empty,
-# then print error message)
+# based on https://unix.stackexchange.com/a/225183,
+# generates loading spinner animation for
+# most recent background process
 
-# Formatted strings
+# ARGUMENTS :
+# $1: loading message
+# $2: successful process message
+# optional $3: error message (requires $4)
+#          $4: error file (if non-empty, print $3)
+
+# formatted strings
 NC="$(tput sgr 0)"
 SPIN_OHNO="[$(tput setaf 1)!$NC]"
-SPIN_OK="[$(tput setaf 34)-$NC]"
+SPIN_OK="[$(tput setaf 2)-$NC]"
 
 loading-spinner () {
-# Assign positional args to variables
-MSG_LOADING="$1"
-MSG_DONE="$2"
-MSG_ERROR="$3"
-ERRORFILE="$4"
-# Fetch PID of most recent background process
-pid=$!
-# Spinner animation frames
-SPIN="-\|/"
-# Makes cursor invisible
-tput civis
-# Index var for the loop
-i=0
-# Animation loop
-while kill -0 $pid 2>/dev/null; do
-	i=$(( (i+1) %4  ))
-	printf "\r[$GRAY${SPIN:$i:1}$NC] $MSG_LOADING"
-	sleep .1
-done
-# Final status message
-if [ -s /tmp/gitup.txt ] && [ -n "$MSG_ERROR" ] && [ -n "$ERRORFILE" ]; then
-	printf "\r$SPIN_OHNO $MSG_ERROR      \n"
-else
-	printf "\r$SPIN_OK $MSG_DONE       \n"
-fi
-# Makes cursor visible
-tput cnorm
+	# assign positional args to variables
+	MSG_LOADING="$1"
+	MSG_DONE="$2"
+	MSG_ERROR="$3"
+	ERRORFILE="$4"
+	# fetch PID of most recent background process
+	pid=$!
+	# spinner animation frames
+	SPIN="-\|/"
+	# makes cursor invisible
+	tput civis
+	# index var for the loop
+	i=0
+	# animation loop
+	while kill -0 $pid 2>/dev/null; do
+		i=$(( (i+1) %4  ))
+		printf "\r[$GRAY${SPIN:$i:1}$NC] $MSG_LOADING"
+		sleep .1
+	done
+	# final status message
+	if [ -s /tmp/gitup.txt ] && [ -n "$MSG_ERROR" ] && [ -n "$ERRORFILE" ]; then
+		printf "\r$SPIN_OHNO $MSG_ERROR      \n"
+	else
+		printf "\r$SPIN_OK $MSG_DONE       \n"
+	fi
+	# makes cursor visible
+	tput cnorm
 }
