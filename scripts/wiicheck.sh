ee7b4f5186824545be0b3682c5b522f111d081d9
massivebird <gdrakemail@gmail.com>
2022-10-15 13:33:48 -0400

    ~ grep searches to account for ./!bios/ directory

--- scripts/wiicheck.sh
+++ scripts/wiicheck.sh
@@ -19,27 +19,27 @@ GCN_IS_GOOD=0
 assign-directories () {
 # if user passed $1...
 if [ -n "$1" ]; then
-	# ... and it contains the correct directories...
-	if [ -d "$1/wbfs" ] && [ -d "$1/games" ]; then
-		# ... then assign directory vars
-		printf "$STATUS_OK $(grep -Eo '\/[^\/]*$' <<< $1) successfully initialized\n"
-		DIR_WII="$1/wbfs"
-		DIR_GCN="$1/games"
-		return
-	fi
-	# $1 exists but doesn't pass the vibe checks, so
-	# exit the script
-	printf "$STATUS_OHNO $(grep -Eo '\/[^\/]*$' <<< $1) is an invalid path. Sorry!\n"
-	exit 1
+   # ... and it contains the correct directories...
+   if [ -d "$1/wbfs" ] && [ -d "$1/games" ]; then
+      # ... then assign directory vars
+      printf "$STATUS_OK $(grep -Eo '\/[^\/]*$' <<< $1) successfully initialized\n"
+      DIR_WII="$1/wbfs"
+      DIR_GCN="$1/games"
+      return
+   fi
+   # $1 exists but doesn't pass the vibe checks, so
+   # exit the script
+   printf "$STATUS_OHNO $(grep -Eo '\/[^\/]*$' <<< $1) is an invalid path. Sorry!\n"
+   exit 1
 fi
 # when $1 does not exist
 # and $DIR_BACKUP is valid...
 if [ -d "$DIR_BACKUP/games" ] && [ -d "$DIR_BACKUP/wbfs" ]; then
-	# ... assign directory vars
-	printf "$STATUS_OK Backup directory initialized\n"
-	DIR_WII="$DIR_BACKUP/wbfs"
-	DIR_GCN="$DIR_BACKUP/games"
-	return
+   # ... assign directory vars
+   printf "$STATUS_OK Backup directory initialized\n"
+   DIR_WII="$DIR_BACKUP/wbfs"
+   DIR_GCN="$DIR_BACKUP/games"
+   return
 fi
 # $1 does not exist and $DIR_BACKUP is uncool, so
 # exit the script
@@ -57,28 +57,28 @@ GAMES_GCN=$(ls -l $DIR_GCN | grep '^d' | wc -l)
 SIZE_TOTAL=$(du -chs $DIR_WII $DIR_GCN | tail -n 1 | grep -oE '(^\w.+)[GM]')
 
 # locates unexpected filename(s)
-BAD_WII=$(find $DIR_WII -empty)$(ls $DIR_WII/*/* | grep -vE '\[([A-Z0-9]{6})\]\/\1\.wbf[s0-9]')
-BAD_GCN=$(find $DIR_GCN -empty)$(ls $DIR_GCN/*/* | grep -vE '\[([A-Z0-9]{6})\]\/game\.iso')
+BAD_WII=$(find $DIR_WII -empty)$(ls $DIR_WII/*/* | grep -vE '\!bios|\[([A-Z0-9]{6})\]\/\1\.wbf[s0-9]')
+BAD_GCN=$(find $DIR_GCN -empty)$(ls $DIR_GCN/*/* | grep -vE '\!bios|\[([A-Z0-9]{6})\]\/game\.iso')
 
 # main output
 if [ -n "$BAD_WII" ]; then
-	printf "$STATUS_OHNO $WII: Unexpected filename(s) detected\n"
-	printf "$BAD_WII\n"
+   printf "$STATUS_OHNO $WII: Unexpected filename(s) detected\n"
+   printf "$BAD_WII\n"
 else
-	printf "$STATUS_OK $WII: Looking good!\n"
-	WII_IS_GOOD=1
+   printf "$STATUS_OK $WII: Looking good!\n"
+   WII_IS_GOOD=1
 fi
 
 if [ -n "$BAD_GCN" ]; then
-	printf "$STATUS_OHNO $GCN: Unexpected filename(s) detected\n"
-	printf "$BAD_GCN\n"
+   printf "$STATUS_OHNO $GCN: Unexpected filename(s) detected\n"
+   printf "$BAD_GCN\n"
 else
-	printf "$STATUS_OK $GCN: Looking fresh!\n"
-	GCN_IS_GOOD=1
+   printf "$STATUS_OK $GCN: Looking fresh!\n"
+   GCN_IS_GOOD=1
 fi
 
 # outputs total games if both Wii can GCN are ok
 if [ $WII_IS_GOOD -eq 1 ] && [ $GCN_IS_GOOD -eq 1 ]; then
-	printf "$STATUS_COOL $(( $GAMES_WII + $GAMES_GCN )) games in $SIZE_TOTAL! Cool!\n"
+   printf "$STATUS_COOL $(( $GAMES_WII + $GAMES_GCN )) games in $SIZE_TOTAL! Cool!\n"
 fi
 exit 0 
