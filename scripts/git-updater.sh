2bfa548ec7ffb3316022f41aafcead5e00b80394
massivebird <gdrakemail@gmail.com>
2022-12-13 14:27:49 -0500

    + slightly longer curl max time

--- scripts/git-updater.sh
+++ scripts/git-updater.sh
@@ -29,7 +29,7 @@ done
 # validates internet connection
 check-connection () {
 # tests connection
-curl --max-time 1.1 -Is https://github.com 1> /dev/null
+curl --max-time 1.3 -Is https://github.com 1> /dev/null
 # if connection is good...
 if [ $? -eq 0 ]; then
    # ... and user wants verbose output...
