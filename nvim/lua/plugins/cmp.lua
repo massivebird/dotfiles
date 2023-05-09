00cae729ae11facae7ac349ed4a4133a02941642
massivebird <gdrakemail@gmail.com>
2023-05-09 19:36:25 -0400

    + luasnip

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -8,8 +8,8 @@ return {
          'hrsh7th/cmp-buffer',
          'hrsh7th/cmp-path',
          'hrsh7th/cmp-cmdline',
-         -- 'L3MON4D3/LuaSnip',
-         -- 'saadparwaiz1/cmp_luasnip',
+         'L3MON4D3/LuaSnip',
+         'saadparwaiz1/cmp_luasnip',
          'onsails/lspkind.nvim'
       },
       config = function()
