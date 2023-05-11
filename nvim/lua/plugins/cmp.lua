89fd427bb0ed655acfc02d7d0bdd8ccd2d8ff734
massivebird <gdrakemail@gmail.com>
2023-05-10 22:31:56 -0400

    + proper luasnip functionality

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -10,9 +10,26 @@ return {
          'hrsh7th/cmp-cmdline',
          {
             "L3MON4D3/LuaSnip",
-            -- follow latest release
-            version = "1.*",
-            build = "make install_jsregexp"
+            dependencies = { "rafamadriz/friendly-snippets" },
+            version = "v1.*",
+            -- build = "make install_jsregexp",
+            config = function()
+
+               local ls   = require("luasnip")
+
+               -- https://evesdropper.dev/files/luasnip/ultisnips-to-luasnip/
+               ls.config.set_config({
+                  history = true, -- keep around last snippet local to jump back
+                  enable_autosnippets = true,
+               })
+
+               require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
+               require("luasnip.loaders.from_vscode").lazy_load()
+
+               vim.keymap.set("i", "<Tab>",   "luasnip#expand_or_jumpable() ? 'luasnip-expand-or-jump' : '<Tab>'", {silent = true})
+               vim.keymap.set("i", "<S-Tab>", ":lua require'luasnip'.jump(-1)")
+
+            end
          },
          'saadparwaiz1/cmp_luasnip',
          'onsails/lspkind.nvim'
@@ -21,6 +38,12 @@ return {
          local lspkind = require('lspkind')
          lspkind.init()
          local cmp = require('cmp')
+         local luasnip = require("luasnip")
+         local has_words_before = function()
+            unpack = unpack or table.unpack
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+         end
          cmp.setup {
             mapping = {
                ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
@@ -55,6 +78,22 @@ return {
                   behavior = cmp.ConfirmBehavior.Replace,
                   select = true,
                },
+               ["<Tab>"] = cmp.mapping(function(fallback)
+                  if luasnip.expand_or_jumpable() then
+                     luasnip.expand_or_jump()
+                  elseif has_words_before() then
+                     cmp.complete()
+                  else
+                     fallback()
+                  end
+               end, { "i", "s" }),
+               ["<S-Tab>"] = cmp.mapping(function(fallback)
+                  if luasnip.jumpable(-1) then
+                     luasnip.jump(-1)
+                  else
+                     fallback()
+                  end
+               end, { "i", "s" }),
             },
 
             -- global sources, order = priority
