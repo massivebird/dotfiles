ab925c03d03f62df0c881379e1c836b4e95e032a
massivebird <gdrakemail@gmail.com>
2023-05-14 20:32:39 -0400

    Refactors wiicheck

--- scripts/wiicheck.sh
+++ scripts/wiicheck.sh
@@ -1,84 +1,77 @@
 #!/usr/bin/env bash
 
-## WIICHECK
-# validates the file system structure for an external
-# storage device to be used by the Wii Homebrew application "USB Loader GX"
+# validates the file system structure for an external storage device
+# for use by the Wii Homebrew application "USB Loader GX"
 
 ## OPTIONAL ARGUMENTS
-# $1: single character drive letter of the Wii external storage device
+# $1
+#    Path to the root of your archive, expects two directories:
+#    1) ROOT/wbfs  containing Wii games
+#    2) ROOT/games containing GameCube games
 
-# load dependencies
+# dependencies
 . ~/.config/scripts/lib/str-main.sh
 . ~/.config/scripts/lib/str-games.sh
 . ~/.config/scripts/lib/dir-games.sh
 
-# status check for after regex evaluations
-WII_IS_GOOD=0
-GCN_IS_GOOD=0
+get-number-of-games()
+{
+   echo $(ls -l $@ | grep '^d' | wc -l)
+}
 
-assign-directories () {
-# if user passed $1...
-if [ -n "$1" ]; then
-   # ... and it contains the correct directories...
-   if [ -d "$1/wbfs" ] && [ -d "$1/games" ]; then
-      # ... then assign directory vars
-      printf "$STATUS_OK $(grep -Eo '\/[^\/]*$' <<< $1) successfully initialized\n"
-      DIR_WII="$1/wbfs"
-      DIR_GCN="$1/games"
+assign-directories()
+{
+
+   # no path provided -> use defaults
+   if [ -z "$1" ] && [ -d "$DIR_BACKUP/games" ] && [ -d "$DIR_BACKUP/wbfs" ]
+   then
+      DIR_WII="$DIR_BACKUP/wbfs"
+      DIR_GCN="$DIR_BACKUP/games"
+      printf "$STATUS_OK Analyzing archive at $DIR_BACKUP\n"
       return
    fi
-   # $1 exists but doesn't pass the vibe checks, so
-   # exit the script
-   printf "$STATUS_OHNO $(grep -Eo '\/[^\/]*$' <<< $1) is an invalid path. Sorry!\n"
+
+   if [ -d "$1/wbfs" ] && [ -d "$1/games" ]; then
+      DIR_WII="$1/wbfs"
+      DIR_GCN="$1/games"
+      printf "$STATUS_OK Analyzing archive at $1\n"
+      return
+   fi
+
+   printf "$STATUS_OHNO Invalid archive path.\n"
    exit 1
-fi
-# when $1 does not exist
-# and $DIR_BACKUP is valid...
-if [ -d "$DIR_BACKUP/games" ] && [ -d "$DIR_BACKUP/wbfs" ]; then
-   # ... assign directory vars
-   printf "$STATUS_OK Backup directory initialized\n"
-   DIR_WII="$DIR_BACKUP/wbfs"
-   DIR_GCN="$DIR_BACKUP/games"
-   return
-fi
-# $1 does not exist and $DIR_BACKUP is uncool, so
-# exit the script
-printf "$STATUS_OHNO Backup directory not found! Sorry."
-exit 1
 } 
 
 assign-directories $@
 
-# number of games per system
-GAMES_WII=$(ls -l $DIR_WII | grep '^d' | wc -l)
-GAMES_GCN=$(ls -l $DIR_GCN | grep '^d' | wc -l)
+NUM_GAMES_TOTAL=$(get-number-of-games $DIR_WII $DIR_GCN)
 
-# calculates total size of all games
 SIZE_TOTAL=$(du -chs $DIR_WII $DIR_GCN | tail -n 1 | grep -oE '(^\w.+)[GM]')
 
-# locates unexpected filename(s)
-BAD_WII=$(find $DIR_WII -empty)$(ls $DIR_WII/*/* | grep -vE '\!bios|\[([A-Z0-9]{6})\]\/\1\.wbf[s0-9]')
-BAD_GCN=$(find $DIR_GCN -empty)$(ls $DIR_GCN/*/* | grep -vE '\!bios|\[([A-Z0-9]{6})\]\/game\.iso')
+BAD_FILES_WII=$(find $DIR_WII -empty)$(ls $DIR_WII/*/* | grep -vE '\!bios|\[([A-Z0-9]{6})\]\/\1\.wbf[s0-9]')
+BAD_FILES_GCN=$(find $DIR_GCN -empty)$(ls $DIR_GCN/*/* | grep -vE '\!bios|\[([A-Z0-9]{6})\]\/game\.iso')
 
-# main output
-if [ -n "$BAD_WII" ]; then
+declare -i WII_IS_GOOD
+declare -i GCN_IS_GOOD
+
+if [ -n "$BAD_FILES_WII" ]; then
    printf "$STATUS_OHNO $WII: Unexpected filename(s) detected\n"
-   printf "$BAD_WII\n"
+   printf "$BAD_FILES_WII\n"
 else
    printf "$STATUS_OK $WII: Looking good!\n"
    WII_IS_GOOD=1
 fi
 
-if [ -n "$BAD_GCN" ]; then
+if [ -n "$BAD_FILES_GCN" ]; then
    printf "$STATUS_OHNO $GCN: Unexpected filename(s) detected\n"
-   printf "$BAD_GCN\n"
+   printf "$BAD_FILES_GCN\n"
 else
    printf "$STATUS_OK $GCN: Looking fresh!\n"
    GCN_IS_GOOD=1
 fi
 
-# outputs total games if both Wii can GCN are ok
 if [ $WII_IS_GOOD -eq 1 ] && [ $GCN_IS_GOOD -eq 1 ]; then
-   printf "$STATUS_COOL $(( $GAMES_WII + $GAMES_GCN )) games in $SIZE_TOTAL! Cool!\n"
+   printf "$STATUS_COOL $NUM_GAMES_TOTAL games in $SIZE_TOTAL! Cool!\n"
 fi
+
 exit 0 
