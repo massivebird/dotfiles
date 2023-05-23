9d2dac076bfe81ee5facd1a0c7babed4f472d3b2
massivebird <gdrakemail@gmail.com>
2023-05-23 00:27:18 -0400

    Adds color preferences

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -6,8 +6,16 @@ return {
          -- https://github.com/NvChad/nvim-colorizer.lua#Customization
          require("colorizer").setup {
             user_default_options = {
+               -- update even when buffer is not focused
                always_update = true,
+               -- "red, "blue"
                names = false,
+               -- #123, #A23
+               RGB = false,
+               -- 0x221100ff
+               AARRGGBB = true,
+               -- #00FF0020
+               RRGGBBAA = true,
             }
          }
       end,
