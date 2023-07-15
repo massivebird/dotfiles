b28e474b2caefbc292655f99cc957bacbf147af2
massivebird <gdrakemail@gmail.com>
2023-07-14 22:15:51 -0400

    changes nixpkgs to unstable

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,7 +2,7 @@
   description = "massivebird's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
