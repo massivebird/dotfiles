fd799402a1071f26c771724e8cb48b09456be96f
massivebird <gdrakemail@gmail.com>
2024-08-08 23:56:50 -0400

    changed: home-manager version -> 24.05

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
