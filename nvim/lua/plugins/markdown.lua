0e5313d1299cf3feca18b4f9fcd03519b091aef3
massivebird <33991373+massivebird@users.noreply.github.com>
2024-09-11 21:26:17 -0400

    changed: a markdown plugin configuration

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,17 +1,35 @@
 return {
 
+
    {
+      -- Header highlights, code block syntax highlighting, etc.
       "preservim/vim-markdown",
       ft = { "markdown" },
       config = function()
          vim.opt.conceallevel = 0
-         -- vim.g["vim_markdown_folding_disabled"] = 0
-         -- vim.g["vim_markdown_folding_level"] = 3
+
+         -- Prevent folding the entire freaking document
+         vim.g["vim_markdown_folding_level"] = 3
+
+         -- Number of spaces per list indent level
          vim.g["vim_markdown_new_list_item_indent"] = 2
+
+         -- Disable plugin keymaps (their `gx` override sucks)
+         vim.g["vim_markdown_no_default_key_mappings"] = 1
+
+         -- Enable pseudo LaTeX syntax highlighting, as in $x^2$
+         vim.g["vim_markdown_math"] = 1
+
+         -- Enable pseudo frontmatter syntax highlighting
+         vim.g["vim_markdown_frontmatter"] = 1
+         vim.g["vim_markdown_toml_frontmatter"] = 1
+
+         -- Enable strikethrough styling
+         vim.g["vim_markdown_strikethrough"] = 1
       end
    },
 
-   -- preview in web browser
+   -- Preview markdown in web browser
    {
       "iamcco/markdown-preview.nvim",
       ft = { "markdown" },
