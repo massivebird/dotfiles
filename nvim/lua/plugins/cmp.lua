b6ce84f4842ae8803677bfd9b568e6dc9d3c7311
massivebird <33991373+massivebird@users.noreply.github.com>
2024-10-30 17:21:00 -0400

    fixed: breaking keymaps configuration

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -12,23 +12,11 @@ return {
       -- build = 'cargo build --release',
 
       opts = {
-         -- for keymap, all values may be string | string[]
-         -- use an empty table to disable a keymap
-         keymap = {
-            show = '<C-space>',
-            hide = '<C-e>',
-            accept = '<Tab>',
-            select_prev = { '<Up>', '<C-k>' },
-            select_next = { '<Down>', '<C-j>' },
-
-            show_documentation = {},
-            hide_documentation = {},
-            scroll_documentation_up = '<C-b>',
-            scroll_documentation_down = '<C-f>',
-
-            snippet_forward = '<Tab>',
-            snippet_backward = '<S-Tab>',
-         },
+         -- 'default' for mappings similar to built-in completion
+         -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+         -- see the "default configuration" section below for full documentation on how to define
+         -- your own keymap. when defining your own, no keybinds will
+         keymap = 'default',
 
          highlight = {
             -- sets the fallback highlight groups to nvim-cmp's highlight groups
