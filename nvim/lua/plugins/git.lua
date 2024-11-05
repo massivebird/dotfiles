29ed11e2194884c8d64411d5b266bc469ba646d9
massivebird <33991373+massivebird@users.noreply.github.com>
2024-11-05 00:41:48 -0500

    wip: changed gitgutter to gitsigns
    Gitsigns is NOT auto-attaching!! It attaches after writing to a file or
    after `:Gitsigns attach`, but not automatically. Annoying!

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,13 +1,19 @@
 return {
 
-   {
-      "airblade/vim-gitgutter",
-      config = function()
-         vim.keymap.set('n', '<leader>gn', ':GitGutterNextHunk<cr>', {silent = true})
-         vim.keymap.set('n', '<leader>gp', ':GitGutterPrevHunk<cr>', {silent = true})
-         vim.keymap.set('n', '<leader>gv', ':GitGutterPreviewHunk<cr>', {silent = true})
-         vim.keymap.set('n', '<leader>gu', ':GitGutterUndoHunk<cr>', {silent = true})
-      end
-   },
+   "https://github.com/lewis6991/gitsigns.nvim",
+   config = function()
+      local gs = require("gitsigns")
+
+      gs.setup {
+         signs = {
+            auto_attach = true,
+         }
+      }
+
+      vim.keymap.set('n', '<leader>gn', gs.next_hunk, { silent = true })
+      vim.keymap.set('n', '<leader>gp', gs.prev_hunk, { silent = true })
+      vim.keymap.set('n', '<leader>gv', gs.preview_hunk, { silent = true })
+      vim.keymap.set('n', '<leader>gu', gs.reset_hunk, { silent = true })
+   end
 
 }
