07935b8090049108b1d57e13e875f3909444597c
massivebird <33991373+massivebird@users.noreply.github.com>
2025-01-06 15:13:31 -0500

    changed(nvim): blink config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -37,18 +37,9 @@ return {
             menu = {
                border = 'rounded',
                min_width = 30,
-               -- max_width = 50,
                max_height = 10,
-               -- keep the cursor X lines away from the top/bottom of the window
                scrolloff = 2,
-               -- which directions to show the window,
-               -- falling back to the next direction when there's not enough space
                direction_priority = { 's', 'n' },
-               -- Controls how the completion items are rendered on the popup window
-               -- 'simple' will render the item's kind icon the left alongside the label
-               -- 'reversed' will render the label on the left and the kind icon + name on the right
-               -- 'minimal' will render the label on the left and the kind name on the right
-               -- 'function(blink.cmp.CompletionRenderContext): blink.cmp.Component[]' for custom rendering
                draw = {
                   columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, },
                },
@@ -56,15 +47,9 @@ return {
 
             list = {
                cycle = {
-                  -- When `true`, calling `select_next` at the *bottom* of the completion list will select the *first* completion item.
                   from_bottom = true,
-                  -- When `true`, calling `select_prev` at the *top* of the completion list will select the *last* completion item.
                   from_top = true
                },
-               -- Controls how the completion items are selected
-               -- 'preselect' will automatically select the first item in the completion list
-               -- 'manual' will not select any item by default
-               -- 'auto_insert' will not select any item by default, and insert the completion items automatically when selecting them
                selection = 'auto_insert',
             },
 
@@ -73,9 +58,6 @@ return {
                auto_show_delay_ms = 200,
                update_delay_ms = 100,
                treesitter_highlighting = true,
-               -- which directions to show the documentation window,
-               -- for each of the possible autocomplete window directions,
-               -- falling back to the next direction when there's not enough space
                window = {
                   min_width = 10,
                   max_width = 60,
@@ -107,9 +89,7 @@ return {
                max_height = 10,
                winhighlight = 'Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder',
             },
-
          }
       }
-
    }
 }
