51f720855eb42a04d74507181eadab1dad255439
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-15 02:34:59 -0400

    style(nvim): formatting

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -3,10 +3,10 @@ return {
    {
       "airblade/vim-gitgutter",
       config = function()
-         vim.keymap.set('n', '<leader>gn', ':GitGutterNextHunk<cr>', {silent = true})
-         vim.keymap.set('n', '<leader>gp', ':GitGutterPrevHunk<cr>', {silent = true})
-         vim.keymap.set('n', '<leader>gv', ':GitGutterPreviewHunk<cr>', {silent = true})
-         vim.keymap.set('n', '<leader>gu', ':GitGutterUndoHunk<cr>', {silent = true})
+         vim.keymap.set('n', '<leader>gn', ':GitGutterNextHunk<cr>', { silent = true })
+         vim.keymap.set('n', '<leader>gp', ':GitGutterPrevHunk<cr>', { silent = true })
+         vim.keymap.set('n', '<leader>gv', ':GitGutterPreviewHunk<cr>', { silent = true })
+         vim.keymap.set('n', '<leader>gu', ':GitGutterUndoHunk<cr>', { silent = true })
       end
    },
 
