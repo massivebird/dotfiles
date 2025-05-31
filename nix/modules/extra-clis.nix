371c6269b0f2cc41bd0af2dc0e112b7a58c2c1a5
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-30 22:17:09 -0400

    added(nix): mutagen pkg

--- nix/modules/extra-clis.nix
+++ nix/modules/extra-clis.nix
@@ -4,6 +4,7 @@
       bun # JavaScript runtime/package manager/bunder/etc.
       gh # github cli
       hyperfine # benchmarking tool written in rust
+      mutagen
       taskwarrior3
       wiki-tui
       yt-dlp
