e9a2e6e9a8dbfd053a7c02c08822f6f0efbbd77b
massivebird <33991373+massivebird@users.noreply.github.com>
2025-08-26 22:56:27 -0400

    added(nix): VirtualBox support for COSC423

--- nix/modules/graphical.nix
+++ nix/modules/graphical.nix
@@ -21,5 +21,13 @@
       wlsunset
       zathura # pdf viewer
     ];
+
+    virtualisation.virtualbox.host.enable = true;
+    users.extraGroups.vboxusers.members = [ "root penguino" ];
+    virtualisation.virtualbox.host.enableExtensionPack = true;
+
+    boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
+    # boot.kernelModules = [ "vboxdrv" "vboxnetadp" "vboxnetflt" ];
+    # boot.extraModulePackages = [ kernelModules ];
   };
 }
