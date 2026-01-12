b119f25d77ca2a4c2a205171ca9a16529e6323f4
massivebird <33991373+massivebird@users.noreply.github.com>
2026-01-11 22:24:02 -0500

    added: cpp flake template

---
+++ flake-templates/cpp.nix
@@ -0,0 +1,54 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # stable Nixpkgs
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                # Override stdenv in order to change compiler:
+                # stdenv = pkgs.clangStdenv;
+              }
+              {
+                packages =
+                  with pkgs;
+                  [
+                    clang-tools
+                    cmake
+                    codespell
+                    cppcheck
+                    doxygen
+                    gtest
+                    lcov
+                    vcpkg
+                    vcpkg-tool
+                  ]
+                  ++ (if stdenv.hostPlatform.system == "aarch64-darwin" then [ ] else [ gdb ]);
+              };
+        }
+      );
+    };
+}
